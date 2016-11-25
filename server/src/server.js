a65b6b5e5f9776aca94980103f351e82249ef1da
Michael Keegan <mekeegan@umass.edu>
2016-11-25 15:38:08 -0500

    static

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,5 @@
-var TestModule = require('./util');
-var reverseString = TestModule.reverseString;
+// var TestModule = require('./util');
+// var reverseString = TestModule.reverseString;
 
 var bodyParser = require('body-parser');
 
@@ -8,9 +8,9 @@ var express = require('express');
 // Creates an Express server.
 var app = express();
 // Defines what happens when it receives the `GET /` request
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
 // Starts the server on port 3000!
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
@@ -18,16 +18,20 @@ app.listen(3000, function () {
 
 app.use(bodyParser.text());
 
-// Handle POST /reverse [data]
-app.post('/reverse', function (req, res) {
-  // If the request came with text, then the text() middleware handled it
-  // and made `req.body` a string.
-  // Check that req.body is a string.
-  if (typeof(req.body) === 'string') {
-    var reversed = reverseString(req.body);
-    res.send(reversed);
-  } else {
-    // POST did not contain a string. Send an error code back!
-    res.status(400).end()
-  }
-});
+// // Handle POST /reverse [data]
+// app.post('/reverse', function (req, res) {
+//   // If the request came with text, then the text() middleware handled it
+//   // and made `req.body` a string.
+//   // Check that req.body is a string.
+//   if (typeof(req.body) === 'string') {
+//     var reversed = reverseString(req.body);
+//     res.send(reversed);
+//   } else {
+//     // POST did not contain a string. Send an error code back!
+//     res.status(400).end()
+//   }
+// });
+
+// You run the server from `server`, so `../client/build` is `server/../client/build`.
+// '..' means "go up one directory", so this translates into `client/build`!
+app.use(express.static('../client/build'));
