62dafac394ad4ddcf2a75159f2298820ac994dc1
Michael Keegan <mekeegan@umass.edu>
2016-11-26 01:13:03 -0500

    almostDone

--- client/app/server.js
+++ client/app/server.js
@@ -6,78 +6,78 @@ var token = 'eyJpZCI6NH0='; // <-- Put your base64'd JSON token here
 * authorization token, and other needed properties.
 */
 function sendXHR(verb, resource, body, cb) {
-var xhr = new XMLHttpRequest();
-xhr.open(verb, resource);
-xhr.setRequestHeader('Authorization', 'Bearer ' + token);
-// The below comment tells ESLint that FacebookError is a global.
-// Otherwise, ESLint would complain about it! (See what happens in Atom if
-// you remove the comment...)
-/* global FacebookError */
-// Response received from server. It could be a failure, though!
-xhr.addEventListener('load', function() {
-var statusCode = xhr.status;
-var statusText = xhr.statusText;
-if (statusCode >= 200 && statusCode < 300) {
-// Success: Status code is in the [200, 300) range.
-// Call the callback with the final XHR object.
-cb(xhr);
-} else {
-// Client or server error.
-// The server may have included some response text with details concerning
-// the error.
-var responseText = xhr.responseText;
-FacebookError('Could not ' + verb + " " + resource + ": Received " +
-statusCode + " " + statusText + ": " + responseText);
-}
-});
-// Time out the request if it takes longer than 10,000
-// milliseconds (10 seconds)
-xhr.timeout = 10000;
-// Network failure: Could not connect to server.
-xhr.addEventListener('error', function() {
-FacebookError('Could not ' + verb + " " + resource +
-": Could not connect to the server.");
-});
-// Network failure: request took too long to complete.
-xhr.addEventListener('timeout', function() {
-FacebookError('Could not ' + verb + " " + resource +
-": Request timed out.");
-});
-switch (typeof(body)) {
-case 'undefined':
-// No body to send.
-xhr.send();
-break;
-case 'string':
-// Tell the server we are sending text.
-xhr.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
-xhr.send(body);
-break;
-case 'object':
-// Tell the server we are sending JSON.
-xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-// Convert body into a JSON string.
-xhr.send(JSON.stringify(body));
-break;
-default:
-throw new Error('Unknown body type: ' + typeof(body));
-}
+  var xhr = new XMLHttpRequest();
+  xhr.open(verb, resource);
+  xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+  // The below comment tells ESLint that FacebookError is a global.
+  // Otherwise, ESLint would complain about it! (See what happens in Atom if
+  // you remove the comment...)
+  /* global FacebookError */
+  // Response received from server. It could be a failure, though!
+  xhr.addEventListener('load', function() {
+    var statusCode = xhr.status;
+    var statusText = xhr.statusText;
+    if (statusCode >= 200 && statusCode < 300) {
+      // Success: Status code is in the [200, 300) range.
+      // Call the callback with the final XHR object.
+      cb(xhr);
+    } else {
+      // Client or server error.
+      // The server may have included some response text with details concerning
+      // the error.
+      var responseText = xhr.responseText;
+      FacebookError('Could not ' + verb + " " + resource + ": Received " +
+      statusCode + " " + statusText + ": " + responseText);
+    }
+  });
+  // Time out the request if it takes longer than 10,000
+  // milliseconds (10 seconds)
+  xhr.timeout = 10000;
+  // Network failure: Could not connect to server.
+  xhr.addEventListener('error', function() {
+    FacebookError('Could not ' + verb + " " + resource +
+    ": Could not connect to the server.");
+  });
+  // Network failure: request took too long to complete.
+  xhr.addEventListener('timeout', function() {
+    FacebookError('Could not ' + verb + " " + resource +
+    ": Request timed out.");
+  });
+  switch (typeof(body)) {
+    case 'undefined':
+    // No body to send.
+    xhr.send();
+    break;
+    case 'string':
+    // Tell the server we are sending text.
+    xhr.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
+    xhr.send(body);
+    break;
+    case 'object':
+    // Tell the server we are sending JSON.
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    // Convert body into a JSON string.
+    xhr.send(JSON.stringify(body));
+    break;
+    default:
+    throw new Error('Unknown body type: ' + typeof(body));
+  }
 }
 /**
 * Emulates a REST call to get the feed data for a particular user.
 */
 export function getFeedData(user, cb) {
-// We don't need to send a body, so pass in 'undefined' for the body.
-sendXHR('GET', '/user/4/feed', undefined, (xhr) => {
-// Call the callback with the data.
-cb(JSON.parse(xhr.responseText));
-});
+  // We don't need to send a body, so pass in 'undefined' for the body.
+  sendXHR('GET', '/user/4/feed', undefined, (xhr) => {
+    // Call the callback with the data.
+    cb(JSON.parse(xhr.responseText));
+  });
 }
 
 /**
- * Emulates how a REST call is *asynchronous* -- it calls your function back
- * some time in the future with data.
- */
+* Emulates how a REST call is *asynchronous* -- it calls your function back
+* some time in the future with data.
+*/
 function emulateServerReturn(data, cb) {
   setTimeout(() => {
     cb(data);
@@ -85,8 +85,8 @@ function emulateServerReturn(data, cb) {
 }
 
 /**
- * Resolves a feed item. Internal to the server, since it's synchronous.
- */
+* Resolves a feed item. Internal to the server, since it's synchronous.
+*/
 function getFeedItemSync(feedItemId) {
   var feedItem = readDocument('feedItems', feedItemId);
   // Resolve 'like' counter.
@@ -102,8 +102,8 @@ function getFeedItemSync(feedItemId) {
 }
 
 /**
- * Emulates a REST call to get the feed data for a particular user.
- */
+* Emulates a REST call to get the feed data for a particular user.
+*/
 // export function getFeedData(user, cb) {
 //
 //     var xhr = new XMLHttpRequest();
@@ -120,20 +120,20 @@ function getFeedItemSync(feedItemId) {
 // }
 
 /**
- * Adds a new status update to the database.
- */
- /**
+* Adds a new status update to the database.
+*/
+/**
 * Adds a new status update to the database.
 */
 export function postStatusUpdate(user, location, contents, cb) {
-sendXHR('POST', '/feeditem', {
-userId: user,
-location: location,
-contents: contents
-}, (xhr) => {
-// Return the new status update.
-cb(JSON.parse(xhr.responseText));
-});
+  sendXHR('POST', '/feeditem', {
+    userId: user,
+    location: location,
+    contents: contents
+  }, (xhr) => {
+    // Return the new status update.
+    cb(JSON.parse(xhr.responseText));
+  });
 }
 // export function postStatusUpdate(user, location, contents, cb) {
 //   // If we were implementing this for real on an actual server, we would check
@@ -174,8 +174,8 @@ cb(JSON.parse(xhr.responseText));
 // }
 
 /**
- * Adds a new comment to the database on the given feed item.
- */
+* Adds a new comment to the database on the given feed item.
+*/
 export function postComment(feedItemId, author, contents, cb) {
   var feedItem = readDocument('feedItems', feedItemId);
   feedItem.comments.push({
@@ -189,44 +189,67 @@ export function postComment(feedItemId, author, contents, cb) {
   emulateServerReturn(getFeedItemSync(feedItemId), cb);
 }
 
+// /**
+//  * Updates a feed item's likeCounter by adding the user to the likeCounter.
+//  * Provides an updated likeCounter in the response.
+//  */
+// export function likeFeedItem(feedItemId, userId, cb) {
+//   var feedItem = readDocument('feedItems', feedItemId);
+//   // Normally, we would check if the user already liked this comment.
+//   // But we will not do that in this mock server.
+//   // ('push' modifies the array by adding userId to the end)
+//   feedItem.likeCounter.push(userId);
+//   writeDocument('feedItems', feedItem);
+//   // Return a resolved version of the likeCounter
+//   emulateServerReturn(feedItem.likeCounter.map((userId) => readDocument('users', userId)), cb);
+// }
+
 /**
- * Updates a feed item's likeCounter by adding the user to the likeCounter.
- * Provides an updated likeCounter in the response.
- */
+* Updates a feed item's likeCounter by adding the user to the likeCounter.
+* Provides an updated likeCounter in the response.
+*/
 export function likeFeedItem(feedItemId, userId, cb) {
-  var feedItem = readDocument('feedItems', feedItemId);
-  // Normally, we would check if the user already liked this comment.
-  // But we will not do that in this mock server.
-  // ('push' modifies the array by adding userId to the end)
-  feedItem.likeCounter.push(userId);
-  writeDocument('feedItems', feedItem);
-  // Return a resolved version of the likeCounter
-  emulateServerReturn(feedItem.likeCounter.map((userId) => readDocument('users', userId)), cb);
+  sendXHR('PUT', '/feeditem/' + feedItemId + '/likelist/' + userId,
+  undefined, (xhr) => {
+    cb(JSON.parse(xhr.responseText));
+  });
 }
 
+// /**
+//  * Updates a feed item's likeCounter by removing the user from the likeCounter.
+//  * Provides an updated likeCounter in the response.
+//  */
+// export function unlikeFeedItem(feedItemId, userId, cb) {
+//   var feedItem = readDocument('feedItems', feedItemId);
+//   // Find the array index that contains the user's ID.
+//   // (We didn't *resolve* the FeedItem object, so it is just an array of user IDs)
+//   var userIndex = feedItem.likeCounter.indexOf(userId);
+//   // -1 means the user is *not* in the likeCounter, so we can simply avoid updating
+//   // anything if that is the case: the user already doesn't like the item.
+//   if (userIndex !== -1) {
+//     // 'splice' removes items from an array. This removes 1 element starting from userIndex.
+//     feedItem.likeCounter.splice(userIndex, 1);
+//     writeDocument('feedItems', feedItem);
+//   }
+//   // Return a resolved version of the likeCounter
+//   emulateServerReturn(feedItem.likeCounter.map((userId) => readDocument('users', userId)), cb);
+// }
+
 /**
- * Updates a feed item's likeCounter by removing the user from the likeCounter.
- * Provides an updated likeCounter in the response.
- */
+* Updates a feed item's likeCounter by removing the user
+* from the likeCounter. Provides an updated likeCounter
+* in the response.
+*/
 export function unlikeFeedItem(feedItemId, userId, cb) {
-  var feedItem = readDocument('feedItems', feedItemId);
-  // Find the array index that contains the user's ID.
-  // (We didn't *resolve* the FeedItem object, so it is just an array of user IDs)
-  var userIndex = feedItem.likeCounter.indexOf(userId);
-  // -1 means the user is *not* in the likeCounter, so we can simply avoid updating
-  // anything if that is the case: the user already doesn't like the item.
-  if (userIndex !== -1) {
-    // 'splice' removes items from an array. This removes 1 element starting from userIndex.
-    feedItem.likeCounter.splice(userIndex, 1);
-    writeDocument('feedItems', feedItem);
-  }
-  // Return a resolved version of the likeCounter
-  emulateServerReturn(feedItem.likeCounter.map((userId) => readDocument('users', userId)), cb);
+  sendXHR('DELETE', '/feeditem/' + feedItemId + '/likelist/' + userId,
+  undefined, (xhr) => {
+    cb(JSON.parse(xhr.responseText));
+  });
 }
 
 /**
- * Adds a 'like' to a comment.
- */
+* Adds a 'like' to a comment.
+*/
 export function likeComment(feedItemId, commentIdx, userId, cb) {
   var feedItem = readDocument('feedItems', feedItemId);
   var comment = feedItem.comments[commentIdx];
@@ -237,8 +260,8 @@ export function likeComment(feedItemId, commentIdx, userId, cb) {
 }
 
 /**
- * Removes a 'like' from a comment.
- */
+* Removes a 'like' from a comment.
+*/
 export function unlikeComment(feedItemId, commentIdx, userId, cb) {
   var feedItem = readDocument('feedItems', feedItemId);
   var comment = feedItem.comments[commentIdx];
@@ -251,66 +274,94 @@ export function unlikeComment(feedItemId, commentIdx, userId, cb) {
   emulateServerReturn(comment, cb);
 }
 
+// /**
+//  * Updates the text in a feed item (assumes a status update)
+//  */
+// export function updateFeedItemText(feedItemId, newContent, cb) {
+//   var feedItem = readDocument('feedItems', feedItemId);
+//   // Update text content of update.
+//   feedItem.contents.contents = newContent;
+//   writeDocument('feedItems', feedItem);
+//   emulateServerReturn(getFeedItemSync(feedItemId), cb);
+// }
+
 /**
- * Updates the text in a feed item (assumes a status update)
- */
+* Updates the text in a feed item (assumes a status update)
+*/
 export function updateFeedItemText(feedItemId, newContent, cb) {
-  var feedItem = readDocument('feedItems', feedItemId);
-  // Update text content of update.
-  feedItem.contents.contents = newContent;
-  writeDocument('feedItems', feedItem);
-  emulateServerReturn(getFeedItemSync(feedItemId), cb);
-}
-
-/**
- * Deletes a feed item.
- */
-export function deleteFeedItem(feedItemId, cb) {
-  // Assumption: The current user authored this feed item.
-  deleteDocument('feedItems', feedItemId);
-  // Remove references to this feed item from all other feeds.
-  var feeds = getCollection('feeds');
-  var feedIds = Object.keys(feeds);
-  feedIds.forEach((feedId) => {
-    var feed = feeds[feedId];
-    var itemIdx = feed.contents.indexOf(feedItemId);
-    if (itemIdx !== -1) {
-      // Splice out of array.
-      feed.contents.splice(itemIdx, 1);
-      // Update feed.
-      writeDocument('feeds', feed);
-    }
+  sendXHR('PUT', '/feeditem/' + feedItemId + '/content', newContent, (xhr) => {
+    cb(JSON.parse(xhr.responseText));
   });
+}
 
-  // Return nothing. The return just tells the client that
-  // the server has acknowledged the request, and that it has
-  // been a success.
-  emulateServerReturn(null, cb);
+// /**
+//  * Deletes a feed item.
+//  */
+// export function deleteFeedItem(feedItemId, cb) {
+//   // Assumption: The current user authored this feed item.
+//   deleteDocument('feedItems', feedItemId);
+//   // Remove references to this feed item from all other feeds.
+//   var feeds = getCollection('feeds');
+//   var feedIds = Object.keys(feeds);
+//   feedIds.forEach((feedId) => {
+//     var feed = feeds[feedId];
+//     var itemIdx = feed.contents.indexOf(feedItemId);
+//     if (itemIdx !== -1) {
+//       // Splice out of array.
+//       feed.contents.splice(itemIdx, 1);
+//       // Update feed.
+//       writeDocument('feeds', feed);
+//     }
+//   });
+//
+//   // Return nothing. The return just tells the client that
+//   // the server has acknowledged the request, and that it has
+//   // been a success.
+//   emulateServerReturn(null, cb);
+// }
+
+/**
+* Deletes a feed item.
+*/
+export function deleteFeedItem(feedItemId, cb) {
+  sendXHR('DELETE', '/feeditem/' + feedItemId, undefined, () => {
+    cb();
+  });
 }
 
 /**
- * Searches for feed items with the given text.
- */
-export function searchForFeedItems(userId, queryText, cb) {
-  // trim() removes whitespace before and after the query.
-  // toLowerCase() makes the query lowercase.
-  queryText = queryText.trim().toLowerCase();
-  var feedId = readDocument('users', userId).feed;
-  var feedItemIDs = readDocument('feeds', feedId).contents;
-  emulateServerReturn(
-    // "filter" is like "map" in that it is a magic method for
-    // arrays. It takes an anonymous function, which it calls
-    // with each item in the array. If that function returns 'true',
-    // it will include the item in a return array. Otherwise, it will
-    // not.
-    // Here, we use filter to return only feedItems that contain the
-    // query text.
-    // Since the array contains feed item IDs, we later map the filtered
-    // IDs to actual feed item objects.
-    feedItemIDs.filter((feedItemID) => {
-      var feedItem = readDocument('feedItems', feedItemID);
-      return feedItem.contents.contents.toLowerCase().indexOf(queryText) !== -1;
-    }).map(getFeedItemSync),
-    cb
-  );
+* Searches for feed items with the given text.
+*/
+export function searchForFeedItems(userID, queryText, cb) {
+  // userID is not needed; it's included in the JSON web token.
+  sendXHR('POST', '/search', queryText, (xhr) => {
+    cb(JSON.parse(xhr.responseText));
+  });
 }
+
+// /**
+//  * Searches for feed items with the given text.
+//  */
+// export function searchForFeedItems(userId, queryText, cb) {
+//   // trim() removes whitespace before and after the query.
+//   // toLowerCase() makes the query lowercase.
+//   queryText = queryText.trim().toLowerCase();
+//   var feedId = readDocument('users', userId).feed;
+//   var feedItemIDs = readDocument('feeds', feedId).contents;
+//   emulateServerReturn(
+//     // "filter" is like "map" in that it is a magic method for
+//     // arrays. It takes an anonymous function, which it calls
+//     // with each item in the array. If that function returns 'true',
+//     // it will include the item in a return array. Otherwise, it will
+//     // not.
+//     // Here, we use filter to return only feedItems that contain the
+//     // query text.
+//     // Since the array contains feed item IDs, we later map the filtered
+//     // IDs to actual feed item objects.
+//     feedItemIDs.filter((feedItemID) => {
+//       var feedItem = readDocument('feedItems', feedItemID);
+//       return feedItem.contents.contents.toLowerCase().indexOf(queryText) !== -1;
+//     }).map(getFeedItemSync),
+//     cb
+//   );
+// }
